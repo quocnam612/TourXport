060710fead7ede6ac2265500df6ddb2ed859429a
Domi3012 <anhkhoi301205@gmail.com>
2026-05-14 12:17:13 +0700

    feat: change the way security.py load env var to get from config.py

--- ai_backend/src/core/security.py
+++ ai_backend/src/core/security.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import jwt
 import os
+from src.core.config import settings
 
 security = HTTPBearer()
 
 def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    public_key = os.getenv("JWT_KEY")
+    jwt_key = settings.jwt_key
     token = credentials.credentials
     try:
-        payload = jwt.decode(token, public_key, algorithms=["HS256"])
+        payload = jwt.decode(token, jwt_key, algorithms=["HS256"])
         return payload
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token đã hết hạn")
