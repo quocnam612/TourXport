64956582b295d116e89cc978f54b25c4811a1933
Nguyễn Quốc Nam <nguyenquocnam612@gmail.com>
2026-05-16 09:27:46 +0700

    refactor ai code for env

--- ai_backend/src/core/config.py
+++ ai_backend/src/core/config.py
@@ -1,13 +1,17 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings(BaseSettings):
-    jwt_key: str ="hello"
-    openai_api_key: str = "hello"
-    mongo_uri: str = "hello"
 
-    # uu tien lay bien env cua docker hon, neu khong co moi lay tu file .env cua thu muc ai_backend
+ROOT_ENV_FILE = Path(__file__).resolve().parents[3] / ".env"
+
+
+class Settings(BaseSettings):
+    jwt_key: str
+    openai_api_key: str
+    mongo_uri: str
+
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=ROOT_ENV_FILE,
         env_file_encoding="utf-8",
         case_sensitive=False,
         extra="ignore"
