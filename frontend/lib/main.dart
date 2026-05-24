40767cea5b42a468f68726fa1b15efea0697f8f1
Vo Minh Khang <voquochuy2210@gmail.com>
2026-05-24 22:07:06 +0700

    Dang nhap vao la vao dashboard luon

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'screens/landing_page.dart';
+import 'screens/sign_in.dart';
 
 void main() {
   runApp(const TourXportApp());
@@ -30,7 +30,7 @@ class TourXportApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const LandingPage(),
+      home: const SignInScreen(),
     );
   }
 }
