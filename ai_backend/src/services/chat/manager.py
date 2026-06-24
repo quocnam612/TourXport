80938ecbcaa5ae03e7327c9050ea26787135ad25
Domi3012 <anhkhoi301205@gmail.com>
2026-06-24 15:48:40 +0700

    migrate from async motor to new pymongo async

--- ai_backend/src/services/chat/manager.py
+++ ai_backend/src/services/chat/manager.py
@@ -1,4 +1,18 @@
-from datetime import datetime, timezone
+from pymongo import AsyncMongoClient
+from openai import AsyncOpenAI
+from uuid import UUID
 
-async def append_message_to_mongo(db, session_id: str, new_message: dict):
-    pass
+class GuideChatService:
+    _db_client: AsyncMongoClient
+    _openai_client: AsyncOpenAI
+    _system_prompt = """
+        (System Prompt đang để lại nghiên cứu vaf viết sao cho tốt nhất)
+    """
+
+    def __init__(self, db_client, openai_client):
+        self._db_client = db_client
+        self._openai_client = openai_client
+    
+    def _get_or_create_history(session_id: UUID):
+        
+        pass
